80b2e274b23b617170ccd58062c9b9cad953ee9a
Kamil Banach <banach.kam@gmail.com>
2019-02-10 13:04:06 +0100

    init git

---
+++ src/main/java/FXApp.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class FXApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        //asdasdas
+        stage.setScene(new Scene(new Pane(), 800, 600));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
